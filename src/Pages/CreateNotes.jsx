eaa4410bfaffb72db09013f4696b3123f19b4157
Whitney Muchira <whitneymuchira@gmail.com>
2023-05-17 07:05:43 +0300

    createNote page styles

--- src/Pages/CreateNotes.jsx
+++ src/Pages/CreateNotes.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Notes from "./Notes";
+import { Link } from "react-router-dom";
+import {IoIosArrowBack} from 'react-icons/io'
 
 const CreateNotes = ({ note, setNote, noteList, setNoteList }) => {
   return(
-<h1>CREATE NOTES</h1>
-  )
+<section>
+<header className="create-note__header">
+  <Link className="btn" link to ="/">
+  <IoIosArrowBack/>
+  </Link>
+  <button className="btn lg primary">Save</button>
+</header>
+<form className="create-note__form">
+<input type="text" autoFocus placeholder="Title" />
+<textarea placeholder="Note Details" rows="30"></textarea>
+</form>
+</section>
+  ) 
 //   const handleChange = (event) => {
 //     setNote(event.target.value);
 //   };
--- src/Pages/EditNotes.jsx
+++ src/Pages/EditNotes.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const EditNotes = () => {
   return (
-    <div>EditNotes</div>
+    
   )
 }
 
