7f860d4323c290d48ce08107ef028e69eb5a5a8e
Whitney Muchira <whitneymuchira@gmail.com>
2023-05-17 09:25:47 +0300

    editNotes styling done

--- src/Pages/EditNotes.jsx
+++ src/Pages/EditNotes.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { IoIosArrowBack } from 'react-icons/io'
+import {RiDeleteBin6Line} from 'react-icons/ri'
+import { Link } from 'react-router-dom'
 
 const EditNotes = () => {
   return (
-    
+    <section>
+<header className="create-note__header">
+  <Link className="btn" link to ="/">
+  <IoIosArrowBack/>
+  </Link>
+  <button className="btn lg primary">Save</button>
+  <button className="btn danger"><RiDeleteBin6Line/></button>
+</header>
+<form className="create-note__form">
+<input type="text" autoFocus placeholder="Title" />
+<textarea placeholder="Note Details" rows="30"></textarea>
+</form>
+</section>
   )
 }
 
